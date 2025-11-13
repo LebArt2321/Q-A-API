f19b7342f68f78afa943bcfd56ace5bec466ddb9
LebArt2321_desktop <LebArt2321@gmail.com>
2025-11-13 12:15:20 +0300

    add .env.example

--- db/db.py
+++ db/db.py
@@ -1,9 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 Base = declarative_base()
 
-DATABASE_URL = "postgresql+psycopg2://postgres:postgres@db:5432/postgres"
+DATABASE_URL = os.getenv('DATABASE_URL')
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
